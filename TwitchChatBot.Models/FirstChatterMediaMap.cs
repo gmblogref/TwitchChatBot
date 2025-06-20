95a6e7d4ee3c8e7c1cf0d4dbc2632c8007c123e8
gmblogref <gmblogref@hotmail.com>
2025-06-20 13:13:01 -0500

    removing json holding

--- TwitchChatBot.Models/FirstChatterMediaMap.cs
+++ TwitchChatBot.Models/FirstChatterMediaMap.cs
@@ -10,19 +10,4 @@
         public string Username { get; set; } = string.Empty;
         public string Media { get; set; } = string.Empty;
     }
-
-    /* PUT INTO JSON FILE WHEN READY
-     * {
-     * "FirstChatterMediaItems": [
-     * { "username": "knee_kap", "media": "first_chat_vods/KneeKap_FirstChat.mp4" },
-     * { "username": "flana", "media": "first_chat_vods/Flana_FirstChat.mp4" },
-     * { "username": "ben324324324", "media": "first_chat_vods/Ben324324324_FirstChat.mp4" },
-     * { "username": "doog", "media": "first_chat_vods/DOOG_FirstChat.mp4" },
-     * { "username": "jmaj7", "media": "first_chat_vods/Major_FirstChat.mp4" },
-     * { "username": "lopez_af", "media": "first_chat_vods/Lopez_FirstChat.mp4" },
-     * { "username": "jillybenilly", "media": "first_chat_vods/Jilly_FirstChat.mp4" },
-     * { "username": "legendofsack", "media": "first_chat_vods/Jilly_FirstChat.mp4" }
-     * ]
-     * }
-     */
 }
