44ae03e6bdf5bcc812c9810f744131b2b0377f15
gmblogref <gmblogref@hotmail.com>
2025-07-31 12:32:00 -0500

    new way to get follows and nugets updates

---
+++ TwitchChatBot.Models/StreamlabsFollowMessage.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+public class StreamlabsFollowWrapper
+{
+    [JsonPropertyName("type")]
+    public string? Type { get; set; }
+
+    [JsonPropertyName("message")]
+    public List<StreamlabsFollowMessage>? Message { get; set; }
+}
+
+public class StreamlabsFollowMessage
+{
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+}
